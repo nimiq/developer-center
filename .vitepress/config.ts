7b9a9b82c02f5fd533fc0dbcea5421d9ea25d875
maximo <maximo@nimiq.com>
2023-10-23 13:14:06 +0200

    add custom title

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,28 +1,101 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
+import { fileURLToPath, URL } from "node:url";
+import AutoImport from "unplugin-auto-import/vite";
+import UnoCSS from "unocss/vite";
+import { presetAttributify, presetIcons, presetUno } from "unocss";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import presetWebFonts from '@unocss/preset-web-fonts'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Nimiq Developer Center",
-  description: "Nimiq's official documentation to interact with the Nimiq ecosystem",
+  description:
+    "Nimiq's official documentation to interact with the Nimiq ecosystem",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: "Examples",
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: "github", link: "https://github.com/nimiq" },
+      { icon: "twitter", link: "https://twitter.com/nimiq" },
+    ],
+  },
+  vite: {
+    plugins: [
+      Components({
+        dirs: ["components"],
+        include: [/\.vue$/, /\.md$/],
+        resolvers: [ElementPlusResolver({ ssr: true })],
+      }),
+      AutoImport({
+        imports: [
+          "vue",
+          "@vueuse/core",
+        ],
+        vueTemplate: true,
+        resolvers: [ElementPlusResolver({ ssr: true })],
+      }),
+      UnoCSS({
+
+        safelist: ["root"],
+        presets: [
+          presetUno(),
+          presetAttributify(),
+          presetIcons(),
+          presetWebFonts({
+            provider: 'bunny',
+            fonts: {
+              sans: 'Mulish:400,600,700',
+              mono: 'Fira Code:400'
+            },
+          }),
+        ],
+        preflights: [
+          {
+            getCSS: ({ theme }) => `
+              html.dark {
+                --vp-c-bg: ${theme.colors["dark-blue"]};
+              }
+              :root {
+                --vp-font-family-base: Mulish,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;
+               --vp-font-family-mono: 'Fira Code', ui-monospace, SFMono-Regular, 'SF Mono', Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace
+              }
+            `,
+          },
+        ],
+        theme: {
+          colors: {
+            "dark-blue": "#1f2348",
+          },
+        },
+        shortcuts: [
+          ["root", "html.dark:[--vp-c-bg:#1f2348]"],
+        ],
+      }),
+    ],
+    resolve: {
+      alias: [
+        {
+          find: /^.*\/VPNavBarTitle\.vue$/,
+          replacement: fileURLToPath(
+            new URL("./components/header/CustomTitle.vue", import.meta.url),
+          ),
+        },
+      ],
+    },
+  },
+});
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,15 +2,17 @@
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import './style.css'
+import 'virtual:uno.css'
 
 export default {
   extends: Theme,
   Layout: () => {
-    return h(Theme.Layout, null, {
+    return h(Theme.Layout, {class: 'root font-sans'}, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      // We also override default components. Refer to https://vitepress.dev/guide/extending-default-theme#overriding-internal-components
     })
   },
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
+  },
 }
