80a580ed3a2ea095e65691d00c18189d1a23e083
maximo <maximo@nimiq.com>
2023-11-11 18:04:11 -0600

    Fixed Grid

--- .vitepress/theme/components/Grid.vue
+++ .vitepress/theme/components/Grid.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Card } from "./Card.vue"
+
 const props = defineProps({
   items: {
     type: Object as PropType<Card[]>,
@@ -8,7 +10,7 @@ const props = defineProps({
 
 const items = computed(() => {
   return props.items.map(i => {
-    const hasColors = i.bgColor && ['blue', 'green'].includes(i.bgColor)
+    const hasColors = i["bg-color"] && ['blue', 'green'].includes(i["bg-color"])
 
     return {
       hasColors,
@@ -22,7 +24,7 @@ const items = computed(() => {
 
 <template>
   <ul class="grid grid-cols-1 md:grid-cols-6 md:grid-rows-[1fr_auto] gap-32">
-    <li v-for="item in items" :style="`grid-column: span ${item.span || 2}`">
+    <li v-for="item in items" :key="item.name" :style="`grid-column: span ${item.span || 2}`">
       <Card :item="item" />
     </li>
   </ul>
