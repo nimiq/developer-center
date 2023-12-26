c66599486e2f2647d784332f48815ce9ccdfd0d0
Maxi <maximogarciamtnez@gmail.com>
2023-12-26 19:17:34 +0100

    Add medium-zoom library for image zoom functionality

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,6 +4,9 @@ import Theme from 'vitepress/theme'
 import './style.css'
 import 'uno.css'
 import DeveloperCenterLayout from './DeveloperCenterLayout.vue'
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+import mediumZoom from 'medium-zoom';
 
 export default {
   extends: Theme,
@@ -12,5 +15,18 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     // ...
+  },
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      mediumZoom('.vp-doc img', { background: 'var(--vp-c-bg)' });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
   },
 }
