d42f2b7738b1f968ccc2cb7e4bf5d261923f47b7
Maxi <maximogarciamtnez@gmail.com>
2024-01-25 16:44:29 +0100

    fixed ui issues

--- .vitepress/theme/components/Tags.vue
+++ .vitepress/theme/components/Tags.vue
@@ -1,23 +1,24 @@
-
 <script setup lang="ts">
-import { PropType } from 'vue'
-import { Tag } from "./types"
+import type { PropType } from 'vue'
+import { Tag } from './types'
 
 defineProps({
   tags: Set as PropType<Set<Tag>>,
 })
 
 const icons = {
-  [Tag.RPC]: 'i-nimiq:nodes',
-  [Tag.WebClient]: 'i-nimiq:globe'
+  [Tag.RPC]: 'i-nimiq:icons-lg-nodes',
+  [Tag.WebClient]: 'i-nimiq:icons-lg-globe',
 }
 </script>
 
 <template>
   <div flex gap-x-16 flex-wrap>
-    <div v-for="tag in tags" :key="tag" flex items-center gap-x-8 p="x-12 y-8"
-      border="1 solid darkblue-25 dark:darkblue-70 inverted:white/35" rounded-6 leading-1>
-      <div :class="icons[tag]" w-14 h-14 text="darkblue-50 inverted:white/60"></div>
+    <div
+      v-for="tag in tags" :key="tag" flex items-center gap-x-8 p="x-12 y-8"
+      border="1 solid darkblue-25 dark:darkblue-70 inverted:white/35" rounded-6 leading-1
+    >
+      <div :class="icons[tag]" w-14 h-14 text="darkblue-50 inverted:white/60" />
       <span text="14 darkblue-60 inverted:white/60">{{ tag }}</span>
     </div>
   </div>
