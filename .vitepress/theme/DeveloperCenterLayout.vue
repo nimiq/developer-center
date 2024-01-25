5168da2a4ab835e14d65b1d1216288fcfc6cabe5
Maxi <maximogarciamtnez@gmail.com>
2024-01-25 16:45:56 +0100

    removed unused code

--- .vitepress/theme/DeveloperCenterLayout.vue
+++ .vitepress/theme/DeveloperCenterLayout.vue
@@ -3,53 +3,59 @@ import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { nextTick, provide } from 'vue'
 
+/**
+ * Credit to [@hooray](https://github.com/hooray)
+ * @see https://github.com/vuejs/vitepress/pull/2347
+ *
+ * Adapted to by an hexagon animation
+ */
+
 const { isDark } = useData()
 const enableTransitions = () => 'startViewTransition' in document && window.matchMedia('(prefers-reduced-motion: no-preference)').matches
 
 function getHexagonPoints(x: number, y: number, r: number): string {
-  const angles = [0, 60, 120, 180, 240, 300];
-  return angles.map(angle => {
-    const radian = (Math.PI / 180) * angle;
-    const dx = r * Math.cos(radian);
-    const dy = r * Math.sin(radian);
-    return `${x + dx}px ${y + dy}px`;
-  }).join(', ');
+  const angles = [0, 60, 120, 180, 240, 300]
+  return angles.map((angle) => {
+    const radian = (Math.PI / 180) * angle
+    const dx = r * Math.cos(radian)
+    const dy = r * Math.sin(radian)
+    return `${x + dx}px ${y + dy}px`
+  }).join(', ')
 }
 
 provide('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
   if (!enableTransitions()) {
-    isDark.value = !isDark.value;
-    return;
+    isDark.value = !isDark.value
+    return
   }
 
   const maxDistance = Math.max(
-    x, 
+    x,
     innerWidth - x,
-    y / Math.sqrt(3), 
+    y / Math.sqrt(3),
     (innerHeight - y) / Math.sqrt(3),
-  );
+  )
 
   const clipPath = [
     `polygon(${getHexagonPoints(x, y, 0)})`,
-    `polygon(${getHexagonPoints(x, y, maxDistance)})`
-  ];
+    `polygon(${getHexagonPoints(x, y, maxDistance)})`,
+  ]
 
   await document.startViewTransition(async () => {
-    isDark.value = !isDark.value;
-    await nextTick();
-  }).ready;
+    isDark.value = !isDark.value
+    await nextTick()
+  }).ready
 
   document.documentElement.animate(
     { clipPath: isDark.value ? clipPath.reverse() : clipPath },
     {
       duration: 300,
       easing: 'ease-in',
-      pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
-    }
-  );
+      pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`,
+    },
+  )
 })
 </script>
-
 
 <template>
   <DefaultTheme.Layout />
