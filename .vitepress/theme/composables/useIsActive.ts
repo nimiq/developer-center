66c643959342f28b56e20dd2b638eb02b30552e8
Maxi <maximogarciamtnez@gmail.com>
2024-01-31 06:42:38 +0100

    fixes ts

---
+++ .vitepress/theme/composables/useIsActive.ts
@@ -0,0 +1,28 @@
+export const HASH_RE = /#.*$/
+export const EXT_RE = /(index)?\.(md|html)$/
+
+export function normalize(path: string): string {
+  return decodeURI(path).replace(HASH_RE, '').replace(EXT_RE, '')
+}
+
+export const inBrowser = typeof document !== 'undefined'
+
+export function isActive(currentPath: string, matchPath?: string, asRegex: boolean = false): boolean {
+  if (matchPath === undefined)
+    return false
+
+  currentPath = normalize(`/${currentPath}`)
+
+  if (asRegex)
+    return new RegExp(matchPath).test(currentPath)
+
+  if (normalize(matchPath) !== currentPath)
+    return false
+
+  const hashMatch = matchPath.match(HASH_RE)
+
+  if (hashMatch)
+    return (inBrowser ? location.hash : '') === hashMatch[0]
+
+  return true
+}
--- vitepress-extension.d.ts
+++ vitepress-extension.d.ts
@@ -1,8 +1,8 @@
-/// <reference path="node_modules/vitepress/types/index.d.ts" />
-
 // rewrite the PageData type from 'vitepress' to add the field `updatedCommitHash`
 declare module 'vitepress' {
-  export interface PageData {
-    updatedCommitHash?: number
+  interface PageData {
+    updatedCommitHash: string
   }
 }
+
+export {}
