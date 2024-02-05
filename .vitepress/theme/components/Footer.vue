d61a982ed4bd9fbf190a067586ee27f0ec9ea181
Maxi <maximogarciamtnez@gmail.com>
2024-02-05 09:31:55 +0100

    working on the footer

--- .vitepress/theme/components/Footer.vue
+++ .vitepress/theme/components/Footer.vue
@@ -1,23 +1,39 @@
+<script setup lang="ts">
+const footer = [
+  {
+    link: 'https://forum.nimiq.community',
+    icon: 'i-nimiq:logos-nimiq-forum',
+    text: 'Question? Checkout the ',
+    social: 'Nimiq Forum',
+  },
+  // {
+  //   link: 'https://youtube.com/@Nimiq',
+  //   icon: 'i-nimiq:logos-youtube',
+  //   text: 'View Developer tutorials on ',
+  //   social: 'Youtube',
+  // },
+  {
+    link: 'https://t.me/nimiq',
+    icon: 'i-nimiq:logos-telegram',
+    text: 'Give us feedback on ',
+    social: 'Telegram',
+  },
+]
+</script>
+
 <template>
   <footer border-top>
     <div px-24 py-48>
       <nav max-w="full sm:640 md:688" mx-auto>
-        <ul flex="~ col gap-16" ml--4>
-          <li>
-            <a href="https://forum.nimiq.community" target="_blank" flex="~ gap-8 items-center" px-4 w-max>
-              <div i-nimiq:logos-nimiq-forum text-17 />
-              <p text="14 darkblue-60">
-                Questions? Check out the <span text-blue font-semibold>Nimiq Forum</span>
-              </p>
-            </a>
-          </li>
-          <li>
-            <a href="https://youtube.com/@Nimiq" target="_blank" flex="~ gap-8 items-center" px-4 w-max>
-              <div i-nimiq:logos-youtube h-24 mr-3 />
-              <p text="14 darkblue-60">
-                View Developer tutorials on <span text-blue font-semibold>Youtube</span>
-              </p>
-            </a>
+        <ul grid="~ col-1 md:cols-2 gap-16" ml--4>
+          <li v-for="(item, i) in footer" :key="item.icon" :class="{ 'md:justify-self-end': i % 2 !== 0 }">
+            <p op70>
+              <a :href="item.link" target="_blank" flex="~ items-center" px-4 w-max un-text="14 !darkblue-60" class="group">
+                <div :class="item.icon" mr-8 />
+                {{ item.text }}<span font-semibold text-blue ml-4>{{ item.social }}</span>
+                <div i-nimiq:arrow-top-right op90 w-8 ml-6 group-hover="translate-x-2 translate-y--2" transition />
+              </a>
+            </p>
           </li>
         </ul>
       </nav>
