bdeb4627151e8af57d49e0fdc45025c5a201d9cd
Maxi <maximogarciamtnez@gmail.com>
2024-02-06 08:20:43 +0100

    handle error in git stats

--- .vitepress/scripts/git-stats.ts
+++ .vitepress/scripts/git-stats.ts
@@ -7,6 +7,7 @@ export async function getGitStats() {
 
   const { hash: albatrossCommitHash, date: albatrossCommitDate } = await fetch('https://api.github.com/repos/nimiq/core-rs-albatross/commits?per_page=1')
     .then(res => res.json()).then(res => ({ hash: res[0].sha.slice(0, 7), date: res[0].commit.author.date }))
+    .catch(() => ({ hash: 'unknown', date: 'unknown' }))
 
   return { repoUrl, commitHash, commitUrl, albatrossCommitHash, albatrossCommitDate }
 }
