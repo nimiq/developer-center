d911499644442c883e59acb64245321e513787ac
Maxi <maximogarciamtnez@gmail.com>
2024-02-06 08:54:49 +0100

    fixes with icons

--- .vitepress/theme/components/Tags.vue
+++ .vitepress/theme/components/Tags.vue
@@ -10,7 +10,7 @@ const tags = computed(() => new Set(props.tags || []))
 
 const icons = {
   [Tag.RPC]: 'i-nimiq:icons-lg-nodes',
-  [Tag.WebClient]: 'i-nimiq:icons-lg-globe',
+  [Tag.WebClient]: 'i-nimiq:globe',
   [Tag.Validator]: 'i-nimiq:icons-lg-verified',
   [Tag.Design]: 'i-nimiq:icons-lg-palette',
 }
