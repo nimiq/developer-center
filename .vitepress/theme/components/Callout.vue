25732938db5be5e732cb5fe4e82335e8ce55d42a
Maxi <maximogarciamtnez@gmail.com>
2024-03-14 15:25:43 +0100

    fix: some ui fixes

--- .vitepress/theme/components/Callout.vue
+++ .vitepress/theme/components/Callout.vue
@@ -4,7 +4,7 @@ const props = defineProps<{ type: 'warning' }>()
 const classes = computed(() => {
   if (props.type === 'warning') {
     return {
-      container: 'bg-orange-400 text-orange border-base-orange',
+      container: 'bg-orange-400 text-orange border-base-orange raw',
       icon: 'i-nimiq:alert',
     }
   }
@@ -17,7 +17,7 @@ const classes = computed(() => {
   <div w-full :class="classes.container" p-24 rounded-6 my-64>
     <div flex="~ gap-8" mb-12>
       <div :class="classes.icon" op80 text-14 />
-      <h5 label text->
+      <h5 label>
         {{ type }}
       </h5>
     </div>
