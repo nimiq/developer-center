e63622ec0179e001d6bb1013a0f913aeb4200224
Janine Wagner <57491220+jvvv-ship-it@users.noreply.github.com>
2024-06-27 12:46:26 +0300

    Fix broken link

--- .vitepress/theme/components/HeadsUp.vue
+++ .vitepress/theme/components/HeadsUp.vue
@@ -5,7 +5,7 @@
       Heads up
     </div>
     <p pr-32 md:pr-64>
-      We are transitioning from a Proof-of-Work blockchain to a Proof-of-Stake blockchain. <a href="https://nimiq.com/developer-center/migration/" underline text-inherit>Learn more about the migration process and how you can get involved.</a> For the latest updates, follow us on <a href="https://twitter.com/Nimiq" underline text-inherit>Twitter</a> or <a href="https://t.me/joinchat/AAAAAEJW-ozFwo7Er9jpHw" underline text-inherit>Telegram</a>.
+      We are transitioning from a Proof-of-Work blockchain to a Proof-of-Stake blockchain. <a :href="withBase('/migration/')" underline text-inherit>Learn more about the migration process and how you can get involved.</a> For the latest updates, follow us on <a href="https://twitter.com/Nimiq" underline text-inherit>Twitter</a> or <a href="https://t.me/joinchat/AAAAAEJW-ozFwo7Er9jpHw" underline text-inherit>Telegram</a>.
     </p>
 
     <div i-custom:crane absolute text-90 md:text-128 right--12 bottom--16 md:top-6 rotate-y-180 op-20 pointer-events-none />
