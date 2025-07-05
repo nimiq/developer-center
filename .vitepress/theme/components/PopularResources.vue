f1b18f911a5ceb3de233045a81360ea2062fc6e8
ines-toupeira <imarques.go@gmail.com>
2025-07-05 09:22:54 +0100

    suggestion: remove bullet points from PopularResources

--- .vitepress/theme/components/PopularResources.vue
+++ .vitepress/theme/components/PopularResources.vue
@@ -28,8 +28,8 @@ defineProps<{
         <h4 text="f-2xs neutral-800" my-0 nq-label>
           {{ resource.title }}
         </h4>
-        <ul text-neutral-400 list-disc space-y-6 f-px-xs f-mt-2xs text="f-sm">
-          <li v-for="(link, index) in resource.links" :key="link.text" ml-0 :mt-0="index === resource.links.length - 1 ? '!' : null">
+        <ul text-neutral-400c space-y-6 f-mt-2xs text="f-sm">
+          <li v-for="(link, index) in resource.links" :key="link.text" :mt-0="index === resource.links.length - 1 ? '!' : null">
             <a :href="link.href" :target="link.target" :class="{ 'nq-arrow after:op-70': link.target === '_blank' }" text-blue f-py-xs hover:underline>{{ link.text }}</a>
           </li>
         </ul>
